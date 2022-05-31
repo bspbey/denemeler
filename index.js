68b4fd0635ad01236836760ccd74a662de233ef3
bspbey <98183612+bspbey@users.noreply.github.com>
2022-05-31 22:54:22 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const discord = require("discord.js-selfbot-v11")
+const stevedore = require("fs")
+
+const client = new discord.Client()
+
+const config = require("./config.json")
+
+const traits = stevedore.readdirSync("./traits/")
+traits.forEach(file => {
+  const traitname = file.split(".")[0]
+  const event = require(`./traits/${file}`);
+  client.on(traitname, event.bind(null,  client));
+})
+
+client.login(process.env.token)
